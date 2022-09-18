08ca48a418412e0826f4fba5b7f219374dd046a9
Diego-Figue <diegofigueroa864@gmail.com>
2022-09-18 10:52:57 -0500

    styled the elements

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+const inputClick = () => {
+  let contain = document.getElementById("idContain");
+  contain.style.transform = "translateY(-250px)";
+  contain.style.transition = "all .5s ease-in-out";
+};
